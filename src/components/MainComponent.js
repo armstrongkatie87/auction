3b32f86a218497fa00e99c79665d75879cc8a598
Katie Armstrong <armstrongkatie87@gmail.com>
2021-12-30 17:22:44 -0800

    Header and Footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import Auction from './AuctionComponent';
 import ProductInfo from './ProductInfoComponent';
 import { PRODUCTS } from '../shared/products';
@@ -20,13 +21,10 @@ class Main extends Component {
     render() {
         return (
             <div>
-                <Navbar dark color="info">
-                    <div className="container">
-                        <NavbarBrand href="/">Pacific Northwest Auction</NavbarBrand>
-                    </div>
-                </Navbar>
-                <Auction products={this.state.products} onClick={productId => this.onProductSelect(productId)}/>
+                <Header />
+                <Auction products={this.state.products} onClick={productId => this.onProductSelect(productId)} />
                 <ProductInfo product={this.state.products.filter(product => product.id === this.state.selectedProduct)[0]} />
+                <Footer />
             </div>
         );
     };
